f37cefebf27f344a77041a1d41b7156397fe569a
DavidCouronne <couronne.david@gmail.com>
2019-01-24 13:16:04 +0100

    Update default.vue

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,102 +1,107 @@
 <template>
+  <!-- <v-app dark> -->
   <v-app>
-    <v-navigation-drawer v-model="drawer" :clipped="clipped" fixed app>
+    <v-navigation-drawer
+      :mini-variant="miniVariant"
+      :clipped="clipped"
+      v-model="drawer"
+      fixed
+      app
+    >
       <v-list>
         <v-list-tile
           v-for="(item, i) in items"
           :to="item.to"
           :key="i"
-          :active-class="color"
           router
           exact
         >
           <v-list-tile-action>
-            <v-icon v-html="item.icon"/>
+            <v-icon v-html="item.icon" />
           </v-list-tile-action>
           <v-list-tile-content>
-            <v-list-tile-title v-text="item.title"/>
+            <v-list-tile-title v-text="item.title" />
           </v-list-tile-content>
         </v-list-tile>
-        <v-divider></v-divider>
-        <Title/>
-        <Sidebar/> 
       </v-list>
     </v-navigation-drawer>
-
-    <v-toolbar :clipped-left="clipped" fixed app dark color="primary">
-      
+    <v-toolbar
+      :clipped-left="clipped"
+      fixed
+      app
+    >
+      <v-toolbar-side-icon @click="drawer = !drawer" />
       <v-btn
-          :color="color"
-          dark
-          icon
-          @click.stop="drawer = !drawer"
-        ><v-icon>reorder</v-icon>
-        </v-btn>
-        <v-btn
-          dark
-          icon
-          router
-          exact
-          to="/"
-        ><v-icon>home</v-icon>
-        </v-btn><v-btn
-          dark
-          icon
-          router
-          exact
-          to="/maths"
-        ><v-icon>school</v-icon>
-        </v-btn>
-        <v-btn
-          dark
-          icon
-          router
-          exact
-          to="/dev"
-        ><v-icon>computer</v-icon>
-        </v-btn>
-
-      <!-- <v-toolbar-title v-text="title"/> -->
-
-      <v-spacer></v-spacer>
+        icon
+        @click.stop="miniVariant = !miniVariant"
+      >
+        <v-icon v-html="miniVariant ? 'chevron_right' : 'chevron_left'" />
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="clipped = !clipped"
+      >
+        <v-icon>web</v-icon>
+      </v-btn>
+      <v-btn
+        icon
+        @click.stop="fixed = !fixed"
+      >
+        <v-icon>remove</v-icon>
+      </v-btn>
+      <v-toolbar-title v-text="title"/>
+      <v-btn
+        icon
+        @click.stop="rightDrawer = !rightDrawer"
+      >
+        <v-icon>menu</v-icon>
+      </v-btn>
     </v-toolbar>
     <v-content>
       <v-container>
-        <nuxt/>
+        <nuxt />
       </v-container>
     </v-content>
-
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019 MIT David Couronné</span>
+    <v-navigation-drawer
+      :right="right"
+      v-model="rightDrawer"
+      temporary
+      fixed
+    >
+      <v-list>
+        <v-list-tile @click.native="right = !right">
+          <v-list-tile-action>
+            <v-icon light>compare_arrows</v-icon>
+          </v-list-tile-action>
+          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
+        </v-list-tile>
+      </v-list>
+    </v-navigation-drawer>
+    <v-footer
+      :fixed="fixed"
+      app
+    >
+      <span>&copy; 2017</span>
     </v-footer>
   </v-app>
 </template>
 
 <script>
-export default {
-  data() {
-    return {
-      activenav: 'secundary',
-      color: 'secondary',
-      clipped: true,
-      drawer: true,
-      fixed: false,
-      isSidebarOpen: false,
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'AcadeTech',
-      items: [
-        { icon: 'home', title: 'Accueil', to: '/' },
-        { icon: 'school', title: 'Mathématiques', to: '/maths' },
-        { icon: 'computer', title: 'Développement', to: '/dev' }
-      ]
-    }
-  },
-  computed: {
-    scrollspy() {
-      return this.$store.state.page.scrollspy
+  export default {
+    data() {
+      return {
+        clipped: false,
+        drawer: true,
+        fixed: false,
+        items: [
+          { icon: 'apps', title: 'Welcome', to: '/' },
+          { icon: 'bubble_chart', title: 'Inspire', to: '/dev' }
+        ],
+        miniVariant: false,
+        right: true,
+        rightDrawer: false,
+        title: 'Vuetify.js'
+      }
     }
   }
-}
 </script>
